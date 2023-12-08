6419e627c4914ff70a8ec5e29f3ea639a18466d2
mveldhuizen <mveldhuizen@e-difference.nl>
2023-12-08 12:39:45 +0100

    Puzzle 1A

---
+++ day_1_a.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * Process the puzzle for AOC Day 1A
+ * @copyright (c) eDifference 2023
+ */
+class Processor
+{
+    /**
+     * Run the puzzle
+     * @return void
+     */
+    public function run(): void
+    {
+        $data = file('data/day_1_a.txt');
+        $counter = 0;
+        foreach ($data as $line) {
+            $counter += $this->getLineValue($line);
+        }
+        echo sprintf("Answer is: %d \n", $counter);
+    }
+
+    /**
+     * Get the first and last digit of a line to get the correct value
+     * @param string $line
+     * @return int
+     */
+    private function getLineValue(string $line): int
+    {
+        preg_match_all("/\d/", $line, $digits);
+        return intval(reset($digits[0]).end($digits[0]));
+    }
+}
+
+(new Processor())->run();
